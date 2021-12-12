c773871da85299647c61ead8a54f4b662c007a90
Fanaen <contact@fanaen.fr>
2021-12-12 18:09:08 +0100

    feat: add software with winget

---
+++ src/winget/install.ps1
@@ -0,0 +1,57 @@
+# Navigateurs
+winget install --id=Mozilla.Firefox --exact --silent
+winget install --id=Mozilla.Firefox.DeveloperEdition --exact --silent
+winget install --id=Google.Chrome --exact --silent
+
+# Media
+winget install --id=Spotify.Spotify --exact --silent
+winget install --id=calibre.calibre --exact --silent
+winget install --id=VideoLAN.VLC --exact --silent
+winget install --id=YACReader.YACReader --exact --silent
+
+# Messagerie
+winget install --id=Telegram.TelegramDesktop --exact --silent
+winget install --id=SlackTechnologies.Slack --exact --silent
+winget install --id=Microsoft.Teams --exact --silent
+winget install --id=Discord.Discord --exact --silent
+
+# Graphisme
+winget install --id=JGraph.Draw --exact --silent # Draw.io
+winget install --id=BlenderFoundation.Blender --exact --silent
+winget install --id=KDE.Krita --exact --silent
+winget install --id=DuongDieuPhap.ImageGlass --exact --silent
+
+# Dev
+winget install --id=Docker.DockerDesktop --exact --silent
+winget install --id=Notepad++.Notepad++ --exact --silent
+winget install --id=Microsoft.VisualStudioCode --exact --silent
+winget install --id=Insomnia.Insomnia --exact --silent
+
+# Terminal
+winget install --id=Microsoft.WindowsTerminal --exact --silent
+winget install --id=Alacritty.Alacritty --exact --silent
+
+# Archive
+winget install --id=7zip.7zip --exact --silent
+
+# Sécurité
+winget install --id=DominikReichl.KeePass --exact --silent
+winget install --id=Cryptomator.Cryptomator --exact --silent
+
+# Transfert / Rangement
+winget install --id=Dropbox.Dropbox --exact --silent
+winget install --id=TimKosse.FileZilla.Client --exact --silent
+winget install --id=WinSCP.WinSCP --exact --silent
+winget install --id=AntSoftware.AntRenamer --exact --silent
+
+# Utilitaire divers
+winget install --id=Ditto.Ditto --exact --silent
+winget install --id=Microsoft.PowerToys --exact --silent
+
+# Système
+winget install --id=CPUID.HWMonitor --exact --silent
+
+# Manquants:
+# - Lightshot
+# - Paint.NET
+# - ExifTool
---
+++ src/winget/update.ps1
@@ -0,0 +1,36 @@
+# Media
+winget upgrade --id=calibre.calibre --exact --silent
+
+# Graphisme
+winget upgrade --id=JGraph.Draw --exact --silent # Draw.io
+winget upgrade --id=DuongDieuPhap.ImageGlass --exact --silent
+winget upgrade --id=BlenderFoundation.Blender --exact --silent
+winget upgrade --id=KDE.Krita --exact --silent
+
+# Dev
+winget upgrade --id=Docker.DockerDesktop --exact --silent
+winget upgrade --id=Notepad++.Notepad++ --exact --silent
+winget upgrade --id=Insomnia.Insomnia --exact --silent
+
+# Terminal
+winget upgrade --id=Microsoft.WindowsTerminal --exact --silent
+winget upgrade --id=Alacritty.Alacritty --exact --silent
+
+# Archive
+winget upgrade --id=7zip.7zip --exact --silent
+
+# Sécurité
+winget upgrade --id=DominikReichl.KeePass --exact --silent
+winget upgrade --id=Cryptomator.Cryptomator --exact --silent
+
+# Transfert
+winget upgrade --id=TimKosse.FileZilla.Client --exact --silent
+winget upgrade --id=WinSCP.WinSCP --exact --silent
+winget upgrade --id=AntSoftware.AntRenamer --exact --silent
+
+# Utilitaire divers
+winget upgrade --id=Ditto.Ditto --exact --silent
+winget upgrade --id=Microsoft.PowerToys --exact --silent
+
+# Système
+winget upgrade --id=CPUID.HWMonitor --exact --silent
